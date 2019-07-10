7293612d25de77dc2a43b2fc4c4d91f62d9ef7c6
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-10 02:42:07 +0000

    Created hello world page, to start testing server

--- express_server.js
+++ express_server.js
@@ -1,7 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8080; // Server Port 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
 
 app.listen(PORT, () => {
   console.log(`funkyBunny listening on port ${PORT}!`);
