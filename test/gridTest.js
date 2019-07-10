ef6714eeccfe507d2f63d21181b2d8845e16d79f
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-10 04:44:50 +0000

    Corrected missing reference to variables in unit tests

--- test/gridTest.js
+++ test/gridTest.js
@@ -2,8 +2,8 @@ const assert = chai.assert;
 
 describe(`generateCoordsArray`, function() {
   it(`returns ['00','01','10','11'] if passed 2`, function() {
-    const seed = 2;
-    const expect = ['00','01','10','11'];
-    assert.deepEqual(generateCoordsArray(2),['00','01','10','11']);
+    const maxValue = 2;
+    const expected = ['00','01','10','11'];
+    assert.deepEqual(generateCoordsArray(maxValue),expected);
   });
 });
