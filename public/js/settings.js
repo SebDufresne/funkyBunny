687439fc0192de491e2d4e4611bbcbc07413c0d2
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-08-03 13:58:34 +0000

    gameState created

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,3 +1,8 @@
+/*
+* Settings for the game
+*/
+
+// Game default values
 const defaults = {
   aiLevel: 'easy',
   boardSize : 10,
@@ -8,9 +13,21 @@ const defaults = {
     3: 2,
     4: 1,
     5: 1
-  }
+  },
+  shotsPerTurn: 1 // not planning on using it
+};
+
+// NOTE: I'll revisit the undefined declarations during implementation
+const gameState = {
+  boardSize: undefined,
+  hitMatrix: [], // see generateMatrix in ai.js
+  movesHistory: [], // [{playerId, position}, ...]
+  players: [], // { {playerId: playerType } (The player type being aiLevel or human/being)
+  roaster: {},
+  shotsPerTurn: undefined,
 };
 
 module.exports = {
-  defaults
+  defaults,
+  gameState
 };
